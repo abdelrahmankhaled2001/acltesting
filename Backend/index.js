bc14c9a9f7e0e455dc3c34680ef501c115e58b26
AmrKollohm <amresmaeelbus@gmail.com>
2022-10-21 14:41:06 +0200

    biiii

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,8 @@
-const express= require('express');
-const app=express();
-const port=process.env.port||4000;
-const connect=require('./config/database.js')
-app.get('/',(req,res)=> res.send('here we go')
-
-);
-
+const express = require("express");
+const app = express();
+const port = process.env.port || 4000;
+const connect = require("./config/database.js");
+app.get("/", (req, res) => res.send("here we go"));
 
 connect();
-app.listen(port)
-
+app.listen(port);
