022b77d86017af5482c0a48b76f4f3fc4075803b
AmrKollohm <amresmaeelbus@gmail.com>
2022-10-21 11:32:33 +0200

    kek

---
+++ Backend/models/Admin.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const adminShema = new mongoose.Shema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
